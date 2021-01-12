d04ceefd773e54b52588213d8338c715fef5cf08
Samuel Davila <samuelrdavila@gmail.com>
2021-01-12 17:08:39 -0600

    added more solutions

--- Basic_Select.sql
+++ Basic_Select.sql
@@ -11,7 +11,7 @@ WHERE (COUNTRYCODE = "USA") and (POPULATION > 100000);
 Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
 */
 SELECT name 
-from CITY
+FROM CITY
 WHERE (COUNTRYCODE = "USA") and (POPULATION > 120000);
 
 /*
@@ -19,6 +19,85 @@ Query all columns (attributes) for every row in the CITY table.
 */
 SELECT *
 FROM CITY;
+
+/*
+Query all columns for a city in CITY with the ID 1661.
+*/
+SELECT * FROM CITY
+WHERE ID = 1661;
+
+/*
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+*/
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = "JPN";
+
+/*
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN. 
+*/
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = "JPN";
+
+/*
+Query a list of CITY and STATE from the STATION table. 
+*/
+SELECT CITY, STATE
+FROM STATION;
+
+/*
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer. 
+*/
+SELECT DISTINCT CITY 
+FROM STATION
+WHERE (ID % 2) = 0;
+
+/*
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically. 
+*/
+SELECT CITY, LENGTH(CITY) 
+FROM STATION
+WHERE LENGTH(CITY) = (SELECT MIN(LENGTH(CITY)) FROM STATION)
+ORDER BY CITY
+LIMIT 1;
+
+SELECT CITY, LENGTH(CITY) 
+FROM STATION
+WHERE LENGTH(CITY) = (SELECT MAX(LENGTH(CITY)) FROM STATION)
+ORDER BY CITY
+LIMIT 1;
+
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE (CITY LIKE "a%") OR (CITY LIKE "e%") OR (CITY LIKE "i%") OR (CITY LIKE "o%") OR (CITY LIKE "u%");
+
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (CITY LIKE '%a') OR (CITY LIKE '%e') OR (CITY LIKE '%i') OR (CITY LIKE '%o') OR (CITY LIKE '%u');
+
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%')
+AND
+(CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u')
+
+/*
+
+*/
+
+/*
+
+*/
 
 /*
 
