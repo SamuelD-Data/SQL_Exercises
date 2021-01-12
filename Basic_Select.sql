c1a104ba781ba1c3a5b7ebeef55f4d1672ca072f
Samuel Davila <samuelrdavila@gmail.com>
2021-01-12 15:52:09 -0600

    updated file name, added new problem

--- Basic_Select.sql
+++ Basic_Select.sql
@@ -15,5 +15,11 @@ from CITY
 WHERE (COUNTRYCODE = "USA") and (POPULATION > 120000);
 
 /*
-Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
+Query all columns (attributes) for every row in the CITY table.
+*/
+SELECT *
+FROM CITY;
+
+/*
+
 */
