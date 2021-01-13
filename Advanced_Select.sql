3358282b828ba934198ece0f26f407253e44661c
Samuel Davila <samuelrdavila@gmail.com>
2021-01-13 06:40:37 -0600

    initial commit

---
+++ Advanced_Select.sql
@@ -0,0 +1,14 @@
+-- SQL Advanced Select --
+
+/*
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
+*/
+SELECT 
+CASE 
+WHEN (A + B > C) AND (B + C > A) AND (A + C > B) THEN 
+CASE 
+WHEN (A = B) AND (B = C) THEN 'Equilateral'
+WHEN (A = B) OR (B = C) OR (A = C) THEN 'Isosceles'
+ELSE 'Scalene' END
+ELSE 'Not A Triangle' END
+FROM TRIANGLES;
