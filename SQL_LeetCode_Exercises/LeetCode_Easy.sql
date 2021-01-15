a24b953e3f82507f707a25fa7ad2bec6607a8afe
Samuel Davila <samuelrdavila@gmail.com>
2021-01-15 13:55:16 -0600

    added more solutions

--- SQL_LeetCode_Exercises/LeetCode_Easy.sql
+++ SQL_LeetCode_Exercises/LeetCode_Easy.sql
@@ -31,24 +31,46 @@ JOIN EMPLOYEE AS B
 WHERE A.MANAGERID = B.ID AND A.SALARY > B.Salary;
 
 /*
-
+Write a SQL query to find all duplicate emails in a table named Person.
 */
+SELECT EMAIL
+FROM PERSON
+GROUP BY EMAIL
+HAVING COUNT(EMAIL) > 1;
 
 /*
-
+Suppose that a website contains two tables, the Customers table and the Orders table. 
+Write a SQL query to find all customers who never order anything.
 */
+SELECT NAME AS CUSTOMERS
+FROM CUSTOMERS
+WHERE ID NOT IN (SELECT CUSTOMERID FROM ORDERS)
 
 /*
-
+Write a SQL query to delete all duplicate email entries in a table named Person, 
+keeping only unique emails based on its smallest Id.
 */
+DELETE PERSON1 
+FROM PERSON AS PERSON1
+JOIN PERSON AS PERSON2
+WHERE (PERSON1.Email = PERSON2.Email) AND (PERSON1.Id > PERSON2.Id)
 
 /*
-
+Write an SQL query to find all dates' id with higher temperature compared to its previous dates (yesterday).
 */
+SELECT W1.ID
+FROM WEATHER AS W1
+JOIN WEATHER AS W2
+WHERE DATEDIFF(W1.RECORDDATE, W2.RECORDDATE) = 1 AND (W1.TEMPERATURE > W2.TEMPERATURE)
 
 /*
-
+There is a table courses with columns: student and class
+Please list out all classes which have more than or equal to 5 students.
 */
+SELECT CLASS
+FROM COURSES
+GROUP BY CLASS
+HAVING COUNT(DISTINCT STUDENT) >= 5;
 
 /*
 
