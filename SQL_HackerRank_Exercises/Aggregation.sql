d4b08f27130d8a97246a04d2f74bf040f94ee637
Samuel Davila <samuelrdavila@gmail.com>
2021-03-18 18:44:46 -0500

    added more solutions

--- SQL_HackerRank_Exercises/Aggregation.sql
+++ SQL_HackerRank_Exercises/Aggregation.sql
@@ -7,3 +7,22 @@ SELECT COUNT(NAME)
 FROM CITY
 WHERE POPULATION > 100000;
 
+/*
+Query the total population of all cities in CITY where District is California.
+*/
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT LIKE 'California';
+
+/*
+Query the average population of all cities in CITY where District is California.
+*/
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT LIKE 'California';
+
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+SELECT ROUND(AVG(POPULATION))
+FROM CITY;
