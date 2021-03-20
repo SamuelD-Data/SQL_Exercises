32ab8b772dd4897a48c6a5b35392051be9d485a5
Samuel Davila <samuelrdavila@gmail.com>
2021-03-19 19:14:38 -0500

    added more solutions

--- SQL_HackerRank_Exercises/Aggregation.sql
+++ SQL_HackerRank_Exercises/Aggregation.sql
@@ -47,3 +47,34 @@ The sum of all values in LONG_W rounded to a scale of  decimal places.
 */
 SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2)
 FROM STATION;
+
+/*
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
+but did not realize her keyboard's 0 key was broken until after completing the calculation. 
+She wants your help finding the difference between her miscalculation (using salaries with any zeroes removed), 
+and the actual average salary.
+Write a query calculating the amount of error (i.e.:  average monthly salaries), 
+and round it up to the next integer.
+*/
+SELECT CEIL(AVG(Salary)-AVG(REPLACE(Salary,'0',''))) FROM EMPLOYEES;
+
+/*
+We define an employee's total earnings to be their monthly salary x months worked, 
+and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
+Write a query to find the maximum total earnings for all employees as well as the total number of employees 
+who have maximum total earnings. Then print these values as  space-separated integers.
+*/
+SELECT SALARY * MONTHS AS MAX_EARN, COUNT(*)
+FROM EMPLOYEE
+GROUP BY MAX_EARN
+ORDER BY MAX_EARN DESC
+LIMIT 1;
+
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and 
+less than 137.2345. Truncate your answer to 4 decimal places.
+*/
+SELECT ROUND(SUM(LAT_N),4)
+FROM STATION
+WHERE LAT_N BETWEEN 38.7880 AND 137.2345;
+
