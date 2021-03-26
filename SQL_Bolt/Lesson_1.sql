c1e2a9acea9003e24b33c08207be51632dc417d1
Samuel Davila <samuelrdavila@gmail.com>
2021-03-26 15:22:13 -0500

    started new sql exercises series, completed lesson 1

---
+++ SQL_Bolt/Lesson_1.sql
@@ -0,0 +1,31 @@
+-- Lesson 1 --
+
+/*
+Find the title of each film 
+*/
+SELECT TITLE 
+FROM MOVIES;
+
+/*
+Find the director of each film 
+*/
+SELECT DIRECTOR 
+FROM MOVIES;
+
+/*
+Find the title and director of each film 
+*/
+SELECT TITLE, DIRECTOR 
+FROM MOVIES;
+
+/*
+Find the title and year of each film
+*/
+SELECT TITLE, YEAR 
+FROM MOVIES;
+
+/*
+Find all the information about each film
+*/
+SELECT * 
+FROM MOVIES;
