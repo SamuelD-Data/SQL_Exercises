46102e7e4b7628f02c5a32171560259454c4823b
Samuel Davila <samuelrdavila@gmail.com>
2021-04-05 12:32:06 -0500

    completed lesson

---
+++ SQL_Bolt/Lesson_18.sql
@@ -0,0 +1,11 @@
+-- Lesson 18 --
+
+/*
+We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+*/
+DROP TABLE IF EXISTS MOVIES;
+
+/*
+And drop the BoxOffice table as well
+*/
+DROP TABLE IF EXISTS BOXOFFICE;
