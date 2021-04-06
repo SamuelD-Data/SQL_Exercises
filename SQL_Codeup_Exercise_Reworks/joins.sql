8e01c9c149c1e4253e52598ab3f34155250e4e9c
Samuel Davila <samuelrdavila@gmail.com>
2021-04-06 17:20:51 -0500

    all exercises done except final bonus

--- SQL_Codeup_Exercise_Reworks/joins.sql
+++ SQL_Codeup_Exercise_Reworks/joins.sql
@@ -22,12 +22,13 @@ where dept_manager.TO_DATE in (SELECT MAX(TO_DATE)
 	  AND employees.GENDER = 'F';
 
 -- Find the current titles of employees currently working in the Customer Service department. --
-SELECT TITLE, DEPT_NAME, titles.EMP_NO, CONCAT(FIRST_NAME, ' ', LAST_NAME) as FULL_NAME
+SELECT TITLE, COUNT(TITLE)
 FROM titles
 JOIN dept_emp on titles.EMP_NO = dept_emp.EMP_NO
 JOIN departments on dept_emp.DEPT_NO = departments.DEPT_NO
 JOIN employees ON dept_emp.EMP_NO = employees.EMP_NO
-where DEPT_NAME = 'CUSTOMER SERVICE' and titles.TO_DATE > CURDATE()
+WHERE DEPT_NAME = 'CUSTOMER SERVICE' and titles.TO_DATE > CURDATE()
+GROUP BY TITLE
 
 -- Find the current salary of all current managers. --
 SELECT FIRST_NAME, LAST_NAME, DEPT_NAME, SALARY
@@ -45,13 +46,61 @@ WHERE dept_manager.TO_DATE IN (SELECT MAX(TO_DATE)
 							   GROUP BY DEPT_NAME);
 
 -- Find the number of current employees in each department. --
+SELECT COUNT(DEPT_NAME), DEPT_NAME
+FROM departments
+JOIN dept_emp on departments.DEPT_NO = dept_emp.DEPT_NO
+JOIN employees on dept_emp.EMP_NO = employees.EMP_NO
+WHERE dept_emp.TO_DATE > CURDATE()
+GROUP BY DEPT_NAME;
 
 -- Which department has the highest average salary? Hint: Use current not historic information. --
+SELECT AVG(SALARY) as AVERAGE_SALARY, dept_name
+FROM salaries
+JOIN dept_emp on salaries.EMP_NO = dept_emp.EMP_NO
+JOIN departments on dept_emp.DEPT_NO = departments.DEPT_NO
+WHERE salaries.TO_DATE > CURDATE()
+GROUP BY dept_name
+ORDER BY AVERAGE_SALARY DESC
+LIMIT 1;
 
 -- Who is the highest paid employee in the Marketing department? --
+SELECT FIRST_NAME, LAST_NAME, SALARY
+FROM salaries
+JOIN dept_emp on salaries.EMP_NO = dept_emp.EMP_NO
+JOIN departments on dept_emp.DEPT_NO = departments.DEPT_NO
+JOIN employees on dept_emp.EMP_NO = employees.EMP_NO
+WHERE salaries.TO_DATE > CURDATE() AND departments.dept_name = 'MARKETING'
+ORDER BY SALARY DESC
+LIMIT 1;
 
 -- Which current department manager has the highest salary? --
+SELECT FIRST_NAME, LAST_NAME, DEPT_NAME, SALARY
+FROM employees
+JOIN dept_manager ON employees.EMP_NO = dept_manager.EMP_NO
+JOIN departments on dept_manager.DEPT_NO = departments.DEPT_NO
+JOIN salaries on employees.EMP_NO = salaries.EMP_NO
+WHERE dept_manager.TO_DATE IN (SELECT MAX(TO_DATE)
+							   FROM departments 
+							   JOIN dept_manager on dept_manager.DEPT_NO = departments.DEPT_NO
+							   GROUP BY DEPT_NAME)
+	  AND salaries.TO_DATE IN (SELECT MAX(TO_DATE)
+							   FROM departments 
+							   JOIN dept_manager on dept_manager.DEPT_NO = departments.DEPT_NO
+							   GROUP BY DEPT_NAME)
+ORDER BY SALARY DESC
+LIMIT 1;
 
 -- Bonus Find the names of all current employees, their department name, and their current manager's name. --
+SELECT FIRST_NAME, LAST_NAME, departments.DEPT_NAME, MANAGERS.MANAGER
+FROM employees
+JOIN dept_emp on employees.EMP_NO = dept_emp.EMP_NO
+JOIN departments on dept_emp.DEPT_NO = departments.DEPT_NO
+JOIN (SELECT DEPT_NAME, CONCAT(FIRST_NAME, ' ', LAST_NAME) as MANAGER
+	FROM employees
+	JOIN dept_emp ON employees.EMP_NO = dept_emp.EMP_NO
+	JOIN departments on dept_emp.DEPT_NO = departments.DEPT_NO
+	JOIN dept_manager on employees.EMP_NO = dept_manager.EMP_NO
+	WHERE dept_manager.TO_DATE > CURDATE()) AS MANAGERS ON departments.DEPT_NAME = MANAGERS.DEPT_NAME
+WHERE dept_emp.TO_DATE > CURDATE();
 
 -- Bonus Who is the highest paid employee within each department. --
