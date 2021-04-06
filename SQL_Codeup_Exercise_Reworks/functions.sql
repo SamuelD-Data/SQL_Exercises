ae682f195b00f3add4db10a32a37d0665900abfa
Samuel Davila <samuelrdavila@gmail.com>
2021-04-06 12:06:43 -0500

    test push due to push error when pushing individual files

---
+++ SQL_Codeup_Exercise_Reworks/functions.sql
@@ -0,0 +1,28 @@
+-- Functions --
+
+/*
+Write a query to to find all employees whose last name starts and ends with 'E'. 
+Use concat() to combine their first and last name together as a single column named full_name.
+*/ 
+
+
+/*
+Convert the names produced in your last query to all uppercase.
+*/
+
+/*
+Find all employees hired in the 90s and born on Christmas. 
+Use datediff() function to find how many days they have been working at the company 
+(Hint: You will also need to use NOW() or CURDATE()),
+*/
+
+/*
+Find the smallest and largest current salary from the salaries table.
+*/
+
+/*
+Use your knowledge of built in SQL functions to generate a username for all of the employees. 
+A username should be all lowercase, and consist of the first character of the employees first name, 
+the first 4 characters of the employees last name, an underscore, the month the employee was born, 
+and the last two digits of the year that they were born. 
+*/
