a3eade93bc45d3661d00a8cd06431340f5231f4d
Samuel Davila <samuelrdavila@gmail.com>
2021-04-07 11:46:38 -0500

    completed remaining exercises in subqueries

--- SQL_Codeup_Exercise_Reworks/subqueries.sql
+++ SQL_Codeup_Exercise_Reworks/subqueries.sql
@@ -49,10 +49,49 @@ WHERE SALARY > (SELECT AVG(SALARY)
 -- How many current salaries are within 1 standard deviation of the current highest salary? --  
 -- (Hint: you can use a built in function to calculate the standard deviation.) -- 
 
+SELECT COUNT(*)
+FROM salaries
+JOIN employees on employees.EMP_NO = salaries.EMP_NO
+WHERE salaries.SALARY >= ((SELECT MAX(SALARY) FROM salaries) - (SELECT STD(SALARY) FROM salaries WHERE TO_DATE > CURDATE()))
+	  AND TO_DATE > CURDATE();
+
 -- What percentage of all salaries is this? -- 
+
+SELECT CONCAT((COUNT(*) / (SELECT COUNT(*) 
+						   FROM salaries 
+						   WHERE TO_DATE > CURDATE())) * 100, '%')
+FROM salaries
+JOIN employees on employees.EMP_NO = salaries.EMP_NO
+WHERE salaries.SALARY >= ((SELECT MAX(SALARY) FROM salaries) - (SELECT STD(SALARY) FROM salaries WHERE TO_DATE > CURDATE()))
+	  AND TO_DATE > CURDATE();
 
 -- Find all the department names that currently have female managers. -- 
 
+SELECT DEPT_NAME, CONCAT(FIRST_NAME, ' ', LAST_NAME) AS MANAGER
+FROM employees
+JOIN dept_emp ON employees.EMP_NO = dept_emp.EMP_NO
+JOIN departments on dept_emp.DEPT_NO = departments.DEPT_NO
+WHERE GENDER = 'F' 
+      AND employees.EMP_NO IN (SELECT emp_no FROM dept_manager WHERE TO_DATE > CURDATE() );
+
 -- Find the first and last name of the employee with the highest salary. -- 
 
+SELECT FIRST_NAME, LAST_NAME
+FROM employees
+JOIN salaries on employees.EMP_NO = salaries.EMP_NO
+WHERE salary = (SELECT MAX(SALARY)
+				FROM salaries
+				WHERE TO_DATE > CURDATE()) 
+	  AND TO_DATE > CURDATE();	
+
 -- Find the department name that the employee with the highest salary works in. -- 
+
+SELECT FIRST_NAME, LAST_NAME, DEPT_NAME
+FROM employees
+JOIN salaries on employees.EMP_NO = salaries.EMP_NO
+JOIN dept_emp on employees.EMP_NO = dept_emp.EMP_NO
+JOIN departments on dept_emp.DEPT_NO = departments.DEPT_NO
+WHERE salary = (SELECT MAX(SALARY)
+				FROM salaries
+				WHERE TO_DATE > CURDATE()) 
+	  AND salaries.TO_DATE > CURDATE();	
