643c98b6eef3fe012172f908dbfbdd239f4d7a61
Samuel Davila <samuelrdavila@gmail.com>
2021-04-15 16:20:41 -0500

    completed all exercises

--- SQL_W3_Schools/select.sql
+++ SQL_W3_Schools/select.sql
@@ -41,3 +41,56 @@ from employees
 
 select count(distinct employee_id)
 from employees
+
+--  Write a query to get the number of jobs available in the employees table. --
+
+select count(distinct job_id)
+from employees
+
+-- Write a query to get all first names from employees table in upper case. -- 
+
+select upper(first_name)
+from employees
+
+-- Write a query to get the first 3 characters of first name from employees table. -- 
+
+select substr(first_name, 1, 3)
+from employees
+
+-- Write a query to calculate 171*214+625. --
+
+select 171*214+625
+
+-- Write a query to get the names (for example Ellen Abel, Sundar Ande etc.) of all the employees from employees table --
+
+select concat(first_name, ' ', last_name)
+from employees
+
+ -- Write a query to get first name from employees table after removing white spaces from both side. --
+
+select trim(first_name)
+from employees
+
+-- Write a query to get the length of the employee names (first_name, last_name) from employees table. --
+
+select length(first_name), length(last_name)
+from employees
+
+-- Write a query to check if the first_name fields of the employees table contains numbers. --
+
+select first_name, case when first_name regexp '[0-9]' then 1 else 0 end as numcheck
+from employees 
+
+-- Write a query to select first 10 records from a table. --
+
+select *
+from employees
+limit 10
+
+-- Write a query to get monthly salary (round 2 decimal places) of each employee --
+-- Note : Assume the salary field provides the 'annual salary' information. --
+
+select round(salary / 12, 2) as monthly_salary
+from employees 
+
+
