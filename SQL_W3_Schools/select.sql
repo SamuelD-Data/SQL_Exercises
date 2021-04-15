5cd609f4683590ef649da19697ec2133d5cfa4f8
Samuel Davila <samuelrdavila@gmail.com>
2021-04-15 13:16:57 -0500

    created w3 folder, added first file and exercise

---
+++ SQL_W3_Schools/select.sql
@@ -0,0 +1,6 @@
+-- Select --
+
+-- Write a query to display the names (first_name, last_name) using alias name “First Name", "Last Name" --
+
+SELECT first_name AS "first name", last_name AS "last name"
+from employees
