65777b160b6af15504db9929fc559898264c72bd
Samuel Davila <samuelrdavila@gmail.com>
2021-04-16 15:13:18 -0500

    more than half of all exercises done

---
+++ SQL_W3_Schools/joins.sql
@@ -0,0 +1,72 @@
+-- Joins --
+
+-- Write a query to find the addresses (location_id, street_address, city, state_province, country_name) of all the departments. --
+
+select location_id, street_address, city, state_province, country_name
+from locations
+join countries on locations.country_id = countries.country_id
+
+-- Write a query to find the name (first_name, last name), department ID and name of all the employees --
+
+select first_name, last_name, employees.department_id, department_name
+from employees
+join departments on employees.department_id = departments.department_id
+
+-- Write a query to find the name (first_name, last_name), job, department ID and name of the employees who works in London. --
+
+select first_name, last_name, job_id, departments.department_id, department_name
+from employees
+join departments on employees.department_id = departments.department_id
+join locations on departments.location_id = locations.location_id
+where city = 'london'
+
+-- Write a query to find the employee id, name (last_name) along with their manager_id and name (last_name). --
+
+select e1.employee_id, e1.last_name, e1.manager_id, e2.last_name as manager_name
+from employees as e1 
+join employees as e2 on e1.manager_id = e2.employee_id
+
+-- Write a query to find the name (first_name, last_name) and hire date of the employees who was hired after 'Jones'. --
+
+select first_name, last_name, e1.hire_date
+from employees as e1
+join (select hire_date from employees where last_name = 'jones') as e2
+where e1.hire_date > e2.hire_date
+
+-- Alternate Solution -- 
+
+select e1.first_name, e1.last_name, e1.hire_date 
+from employees as e1 
+join employees as e2 
+on (e2.last_name = 'Jones') 
+where e1.hire_date > e2.hire_date
+
+-- Write a query to get the department name and number of employees in the department. --
+
+select count(distinct employee_id), department_name
+from employees
+join departments on employees.department_id = departments.department_id
+group by department_name
+
+-- Write a query to find the employee ID, job title, number of days between ending date and starting date for all jobs in department 90. --
+
+select employees.employee_id, employees.job_id, end_date - start_date
+from employees
+join job_history on employees.employee_id = job_history.employee_id
+where employees.department_id = 90
+
+-- Write a query to display the department ID and name and first name of manager. --
+
+select departments.department_id, departments.department_name, first_name
+from employees
+join departments on employees.employee_id = departments.manager_id
+
+-- Write a query to display the department name, manager name, and city. --
+
+select departments.department_name, first_name, last_name, city
+from employees
+join departments on employees.employee_id = departments.manager_id
+join locations on departments.location_id = locations.location_id
+
+-- 
+
