7ac50f6b28b5fe7684ae0c3ae2e66a0faad37f03
Samuel Davila <samuelrdavila@gmail.com>
2021-04-16 18:59:39 -0500

    completed half of exercises

--- SQL_W3_Schools/string_functions.sql
+++ SQL_W3_Schools/string_functions.sql
@@ -25,3 +25,40 @@ where phone_number LIKE '%124%';
 
 -- Write a query to get the details of the employees where the length of the first name greater than or equal to 8. --
 
+select *
+from employees
+where length(first_name) >= 8
+
+-- Write a query to display leading zeros before maximum and minimum salary. --
+
+select job_id,  lpad( max_salary, 7, '0'), lpad( min_salary, 7, '0')
+FROM jobs
+
+-- Write a query to append '@example.com' to email field. --
+
+select concat(email, '@example.com')
+from employees
+
+-- Write a query to get the employee id, first name and hire month. --
+
+select employee_id, first_name, month(hire_date)
+from employees
+
+-- Write a query to get the employee id, email id (discard the last three characters). --
+
+select employee_id, left(email, length(email) - 3)
+from employees
+
+-- Write a query to find all employees where first names are in upper case. --
+
+select first_name, last_name
+from employees 
+where first_name = binary upper(first_name)
+
+
+-- Write a query to extract the last 4 character of phone numbers --
+
+select right(phone_number, 4), phone_number
+from employees
+
+-- 
