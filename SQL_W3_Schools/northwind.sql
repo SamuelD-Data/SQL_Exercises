23af050156aeba6ef834797f2cd50c8ab238eca7
Samuel Davila <samuelrdavila@gmail.com>
2021-04-17 06:16:03 -0500

    half of exercises complete

---
+++ SQL_W3_Schools/northwind.sql
@@ -0,0 +1,51 @@
+-- Northwind --
+
+-- Write a query to get Product name and quantity/unit.  --
+
+select productname, quantityperunit
+from products
+
+-- Write a query to get current Product list (Product ID and name). -- 
+
+select productid, productname
+from products
+
+-- Write a query to get discontinued Product list (Product ID and name). -- 
+
+select productid, productname
+from products
+where discontinued = 1
+
+-- Write a query to get most expense and least expensive Product list (name and unit price). -- 
+
+select productname, max(unitprice), min(unitprice)
+from products
+where unitprice = (select max(unitprice) from products)
+OR unitprice = (select min(unitprice) from products)
+
+-- Write a query to get Product list (id, name, unit price) where current products cost less than $20. -- 
+
+select productid, productname, unitprice
+from products
+where unitprice < 20
+
+-- Write a query to get Product list (id, name, unit price) where products cost between $15 and $25. -- 
+
+select productid, productname, unitprice
+from products
+where unitprice between 15 and 25
+
+-- Write a query to get Product list (name, unit price) of above average price. -- 
+
+
+
+-- Write a query to get Product list (name, unit price) of ten most expensive products. -- 
+
+
+
+-- Write a query to count current and discontinued products. -- 
+
+
+
+-- Write a query to get Product list (name, units on order , units in stock) --
+-- of stock is less than the quantity on order. -- 
