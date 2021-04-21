a73b70d389b1daf52b58cc0c6979fe41f91af4b4
Samuel Davila <samuelrdavila@gmail.com>
2021-04-21 17:07:49 -0500

    completed all exercises

--- SQL_HackerRank_Exercises/aggregation_v2.sql
+++ SQL_HackerRank_Exercises/aggregation_v2.sql
@@ -64,3 +64,55 @@ join (select max(salary * months) as max_sal from employee) as max_sal_col
 where salary * months = max_sal_col.max_sal
 group by max_sal
 
+/*
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of  decimal places.
+The sum of all values in LONG_W rounded to a scale of  decimal places.
+*/
+
+select round(sum(lat_n), 2), round(sum(long_w), 2)
+from station
+
+-- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
+-- Truncate your answer to 4 decimal places. --
+
+select round(sum(lat_n), 4)
+from station
+where lat_n between 38.7880 and 137.2345
+
+-- Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places. --
+
+select round(max(lat_n), 4)
+from station
+where lat_n < 137.2345
+
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. --
+-- Round your answer to 4 decimal places. --
+
+select round(long_w, 4)
+from station
+where lat_n = (select max(lat_n) from station where lat_n < 137.2345)
+
+-- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places. --
+
+select round(min(lat_n), 4)
+from station
+where lat_n > 38.7780
+
+-- Query the Western Longitude (LONG_W) where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. --
+-- Round your answer to 4 decimal places. --
+
+select round(long_w, 4)
+from station
+where lat_n = (select min(lat_n) from station where lat_n > 38.7780 )
+
+-- Query the Manhattan Distance between points p1 and p2 and round it to a scale of 4 decimal places. --
+
+select round(abs(min(lat_n) - max(lat_n)) + abs(min(long_w) - max(long_w)), 4)
+from station
+
+-- Query the Euclidean Distance between points p1 and p2 and format your answer to display 4 decimal digits. --
+
+select round(sqrt(power(min(lat_n) - max(lat_n), 2) + power(min(long_w) - max(long_w), 2)), 4)
+from station
+
